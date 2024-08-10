2af505cb8ba55bff470b6df47ca092b1ac731de7
Nazanin Valaee <61946916+nazaninvalaee@users.noreply.github.com>
2024-08-10 09:56:48 +0330

    Update layer_4_no_mod.py

--- Models/layer_4_no_mod.py
+++ Models/layer_4_no_mod.py
@@ -1,5 +1,5 @@
 from tensorflow.keras import models, layers
-from Models.attention_blocks import ChannelAttention, SpatialAttention
+from Models.attention_blocks import channel_attention, spatial_attention
 
 def create_model(ensem=0):
 
